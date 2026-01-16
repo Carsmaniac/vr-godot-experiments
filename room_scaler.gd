33dd0a1e9fe7ba18bf96668f6e72c9842cfa3c73
Emjay <mickledapickle@me.com>
2026-01-16 18:25:07 +1100

    Add pickable objects and more mini-experiments
    Added an object picking script and attached it to a grey cube in the room. Also added an experiment that grows and shrinks depending on which gesture the user does, and started working on an experiment that grows and shrinks based on which direction the user is pointing

--- room_scaler.gd
+++ room_scaler.gd
@@ -3,15 +3,26 @@ extends Node3D
 var left_hand_closed: bool = false
 var right_hand_closed: bool = false
 
-var mini_experiment: int = 1
-# 1: greater variations in user scale
-# 2: user scale that changes over time
+var l_pointing: bool = false
+var l_thumbing: bool = false
+var r_pointing: bool = false
+var r_thumbing: bool = false
+
+var constant_size_change = 1.007
+var step_size_change = 1.2
+var point_direction_threshold = 70
+
+var mini_experiment: int = 8
+# 1: greater variations in user scale (fist)
+# 2: user scale that changes over time (fist)
 # 3: (not possible in Godot?)
 # 6: changing object scales disproportionally
-# 7: changing scale in reaction to user actions
+# 7: changing scale in reaction to user actions (point and thumbs up)
+# 8: changing scale in reaction to user actions (point up and down)
 
-@export var origin_node: Node
-
+@export var origin_node: Node3D
+@export var left_hand: Node3D
+@export var right_hand: Node3D
 
 
 func _ready() -> void:
@@ -20,10 +31,27 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if mini_experiment == 2:
 		if left_hand_closed:
-			rescale(1/1.01)
+			rescale(constant_size_change)
 		elif right_hand_closed:
-			rescale(1.01)
-	pass
+			rescale(1/constant_size_change)
+			
+	elif mini_experiment == 7:
+		if l_pointing:
+			rescale(constant_size_change)
+		elif l_thumbing:
+			rescale(1/(constant_size_change))
+		
+		if r_pointing:
+			rescale(constant_size_change)
+		elif r_thumbing:
+			rescale(1/(constant_size_change))
+			
+	elif mini_experiment == 8:
+		if l_pointing:
+			if left_hand.rotation.y > point_direction_threshold:
+				rescale(1/(constant_size_change))
+			elif left_hand.rotation.y < -point_direction_threshold:
+				rescale(constant_size_change)
 		
 func rescale(rescale_factor: float):
 	self.scale *= rescale_factor
@@ -36,21 +64,32 @@ func rescale(rescale_factor: float):
 	# 6 months stuck on this and it was this straightforward, oops
 
 
-
 func _on_left_pose_started(p_name: String) -> void:
 	if p_name == "Fist":
 		left_hand_closed = true
 		if mini_experiment == 1:
-			rescale(1/1.2)
+			rescale(1/step_size_change)
+	elif p_name == "Point":
+		l_pointing = true
+	elif p_name == "ThumbsUp":
+		l_thumbing = true
 
 func _on_left_pose_ended(_p_name: String) -> void:
 	left_hand_closed = false
+	l_pointing = false
+	l_thumbing = false
 
 func _on_right_pose_started(p_name: String) -> void:
 	if p_name == "Fist":
 		right_hand_closed = true
 		if mini_experiment == 1:
-			rescale(1.2)
+			rescale(step_size_change)
+	elif p_name == "Point":
+		r_pointing = true
+	elif p_name == "ThumbsUp":
+		r_thumbing = true
 
 func _on_right_pose_ended(_p_name: String) -> void:
 	right_hand_closed = false
+	r_pointing = false
+	r_thumbing = false
